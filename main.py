2669a6d34415ae3b52f7a40093d0b4ded3abcbcd
Nikita Anreychuk <andreychuk333@gmail.com>
2024-04-14 23:03:14 +0300

    init

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+#3-ая задача
 def canJump(nums):
     max_reach = 0
-
     for i in range(len(nums)):
         if i > max_reach:
             return False
@@ -9,4 +9,5 @@ def canJump(nums):
 
 nums1 = [2, 3, 1, 1, 4]
 print(canJump(nums1))
+
 
